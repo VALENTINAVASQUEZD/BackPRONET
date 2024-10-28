a585046e92378a2f468b7d4368a8064813694f48
Jhon Moran <jhon.moran@campusucc.edu.co>
2024-10-28 18:54:57 -0500

    guardar cambio pequeños

--- usuarios/views.py
+++ usuarios/views.py
@@ -8,11 +8,9 @@ class RegistroUsuarioAPIView(APIView):
     def post(self, request):
         serializer = RegistroUsuarioSerializer(data=request.data)
         if serializer.is_valid():
-            # Guardar el usuario en la base de datos
             usuario = serializer.save()
             return Response(
                 {'mensaje': 'Usuario registrado exitosamente'},
                 status=status.HTTP_201_CREATED
             )
-        # Si el serializador no es válido, devolver errores
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
