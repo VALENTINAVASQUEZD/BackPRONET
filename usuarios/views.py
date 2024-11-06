a0bce3e6d4c0c7d4e0863de0f838bce5a1c3a6a2
valentina <mavalede@gmail.com>
2024-11-06 12:54:57 -0500

    verificaciones

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,9 +2,8 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from .serializers import RegistroUsuarioSerializer
-from django.contrib.auth.models import User
-from .models import PerfilUsuario
+from .serializer import RegistroUsuarioSerializer
+
 
 class RegistroUsuarioAPIView(APIView):
 
