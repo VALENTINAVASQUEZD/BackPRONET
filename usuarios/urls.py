b95663cea95635b2033ec9e6d7d2bf3b27049760
Jhon Moran <jhon.moran@campusucc.edu.co>
2024-11-06 13:35:11 -0500

    subiendo url comentada

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import RegistroUsuarioAPIView, LoginAPIView, PerfilUsuarioAPIView
+from .views import RegistroUsuarioAPIView, LoginAPIView
 
 urlpatterns = [
     path('registro/', RegistroUsuarioAPIView.as_view(), name='registro_usuario'),
     path('login/', LoginAPIView.as_view(), name='login_usuario'),
-    path('perfil/', PerfilUsuarioAPIView.as_view(), name='perfil_usuario'),
+    #path('perfil/', PerfilUsuarioAPIView.as_view(), name='perfil_usuario'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from .serializer import LoginSerializer, UsuarioSerializer, RegistroUsuarioSerializer
+from .serializer import LoginSerializer, RegistroUsuarioSerializer
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from .models import PerfilUsuario
