8fe7d1b719692a858f9f2e67f53ed893ff90efe0
Jhon Fredy Moran Quetamá <jhon.moran@campusucc.edu.co>
2024-11-20 05:18:49 +0000

    Deleted interaccionPerfil

--- interaccionPerfil/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.core.exceptions import ValidationError
-from datetime import date
-
-def validar_edad_minima(fecha_nacimiento):
-    hoy = date.today()
-    edad = hoy.year - fecha_nacimiento.year - ((hoy.month, hoy.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
-    if edad < 13:
-        raise ValidationError('El usuario debe tener al menos 13 años de edad.')
