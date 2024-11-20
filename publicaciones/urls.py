2db7830df0353237ac3d96a54a2ef7207ae4a720
Jhon Fredy Moran Quetamá <jhon.moran@campusucc.edu.co>
2024-11-20 05:19:04 +0000

    Deleted publicaciones

--- publicaciones/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ListarCrearPublicacionesView
-
-urlpatterns = [
-    path('', ListarCrearPublicacionesView.as_view(), name='listar_crear_publicaciones'),
-]
