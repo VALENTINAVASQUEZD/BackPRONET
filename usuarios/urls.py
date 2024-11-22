e6e337f314115c045155ed790facdd816a851b56
Jhon Moran <jhon.moran@campusucc.edu.co>
2024-11-22 04:47:12 -0500

    academica listar

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('perfil/<int:user_id>/', PerfilUsuarioAPIView.as_view(), name='perfil_usuario_by_id'),
     path('listar/', ListarUsuariosAPIView.as_view(), name='listar_usuarios'),
     path('informacion-academica/', InformacionAcademicaAPIView.as_view(), name='informacion_academica'),
+    path('listar/informacion-academica/<int:user_id>/', InformacionAcademicaAPIView.as_view(), name='informacion_academica'),
     path('informacion-laboral/', InformacionLaboralAPIView.as_view(), name='informacion_laboral'),
     path('listar/informacion-laboral/<int:user_id>/', InformacionLaboralAPIView.as_view(), name='informacion-laboral'), #get
 ]
